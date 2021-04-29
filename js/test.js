e622d6fda934f08d45a04bc2301f75e26618dc3b
keita_eguchi <35883400+eggchi21@users.noreply.github.com>
2021-04-30 00:58:13 +0900

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,10 +1,15 @@
+import { Board } from './board.js';
+import { MyCanvas } from './canvas.js';
+import { LifeWorld } from './lifeWorld.js';
 export class Program {
-    constructor() {
-
+    constructor(width, height) {
+      var board = new Board(width, height);
+      var canvas = new MyCanvas( 'mycanvas', width, height);
+      this.world = new LifeWorld(board, canvas);
     }
 
   run() {
-    alert('lo');
+    alert('op');
     };
 
 }
@@ -12,7 +17,7 @@ export class Program {
 export function main() {
   alert('okkk');
   window.onload = function () {
-    var program = new Program();
+    var program = new Program(50, 35);
     program.run();
   };
 }
