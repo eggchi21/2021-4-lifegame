0b8e674ffb16b58f1165fe4b5d6446db4549a6dd
keita_eguchi <35883400+eggchi21@users.noreply.github.com>
2021-04-29 23:39:13 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,82 +1,82 @@
-import { Board } from './board.js';
-import { MyCanvas } from './canvas.js';
-import { LifeWorld } from './lifeWorld.js';
+// import { Board } from './board.js';
+// import { MyCanvas } from './canvas.js';
+// import { LifeWorld } from './lifeWorld.js';
 
-export class Program {
-    constructor(width, height) {
-        var board = new Board(width, height);
-        var canvas = new MyCanvas( 'mycanvas', width, height);
-        this.world = new LifeWorld(board, canvas);
+// export class Program {
+//     constructor(width, height) {
+//         var board = new Board(width, height);
+//         var canvas = new MyCanvas( 'mycanvas', width, height);
+//         this.world = new LifeWorld(board, canvas);
 
-        this.dragState = 0;
+//         this.dragState = 0;
 
-        canvas.onMouseDown = (x, y, shiftKey) => {
-            this.dragState = 1;
-        };
+//         canvas.onMouseDown = (x, y, shiftKey) => {
+//             this.dragState = 1;
+//         };
 
-        canvas.onMouseMove = (x, y, shiftKey) => {
-            if (this.dragState >= 1) {
-                if (shiftKey)
-                    board.clear(board.toIndex(x, y));
-                else
-                    board.set(board.toIndex(x, y));
-                this.dragState = 2;
-            }
-        };
+//         canvas.onMouseMove = (x, y, shiftKey) => {
+//             if (this.dragState >= 1) {
+//                 if (shiftKey)
+//                     board.clear(board.toIndex(x, y));
+//                 else
+//                     board.set(board.toIndex(x, y));
+//                 this.dragState = 2;
+//             }
+//         };
 
-        canvas.onMouseUp = (x, y, shiftKey) => {
-            if (this.dragState === 1) {
-                board.reverse(board.toIndex(x, y));
-            }
-            this.dragState = 0;
-        };
+//         canvas.onMouseUp = (x, y, shiftKey) => {
+//             if (this.dragState === 1) {
+//                 board.reverse(board.toIndex(x, y));
+//             }
+//             this.dragState = 0;
+//         };
 
-    }
+//     }
 
-    run() {
-        document.getElementById('startButton').addEventListener('click', () => this.start(), false);
-        document.getElementById('stopButton').addEventListener('click', () => this.stop(), false);
-        document.getElementById('clearButton').addEventListener('click', () => this.clear(), false);
-        document.getElementById('randomButton').addEventListener('click', () => this.random(), false);
-        document.getElementById('createBlockButton').addEventListener('click', () => this.createBlock(), false);
-        document.getElementById('createBlinkerButton').addEventListener('click', () => this.createBlinker(), false);
-        document.getElementById('createGliderButton').addEventListener('click', () => this.createGlider(), false);
-    };
+//     run() {
+//         document.getElementById('startButton').addEventListener('click', () => this.start(), false);
+//         document.getElementById('stopButton').addEventListener('click', () => this.stop(), false);
+//         document.getElementById('clearButton').addEventListener('click', () => this.clear(), false);
+//         document.getElementById('randomButton').addEventListener('click', () => this.random(), false);
+//         document.getElementById('createBlockButton').addEventListener('click', () => this.createBlock(), false);
+//         document.getElementById('createBlinkerButton').addEventListener('click', () => this.createBlinker(), false);
+//         document.getElementById('createGliderButton').addEventListener('click', () => this.createGlider(), false);
+//     };
 
-    start() {
-        this.world.start();
-    };
+//     start() {
+//         this.world.start();
+//     };
 
-    stop() {
-        this.world.stop();
-    };
+//     stop() {
+//         this.world.stop();
+//     };
 
-    clear() {
-        this.world.stop();
-        this.world.clear();
-    };
+//     clear() {
+//         this.world.stop();
+//         this.world.clear();
+//     };
 
-    random() {
-        this.world.random();
-    };
+//     random() {
+//         this.world.random();
+//     };
 
-    createBlock() {
-        this.world.createBlock();
-    }
+//     createBlock() {
+//         this.world.createBlock();
+//     }
 
-    createBlinker() {
-        this.world.createBlinker();
-    }
+//     createBlinker() {
+//         this.world.createBlinker();
+//     }
 
-    createGlider() {
-        this.world.createGlider();
-    }
-}
+//     createGlider() {
+//         this.world.createGlider();
+//     }
+// }
 
 export function main() {
     window.onload = function () {
-        alert('ok');
-        var program = new Program(50, 35);
-        program.run();
+        alert('ng');
+        // var program = new Program(50, 35);
+        // program.run();
     };
 }
